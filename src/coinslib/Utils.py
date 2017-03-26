10b8b053268a0964662d078ebe41c6ab93029625
Francois Chagnon <francois.chagnon@shopify.com>
2017-03-26 11:01:59 -0400

    PEP8-ify coinslib

--- src/coinslib/Utils.py
+++ src/coinslib/Utils.py
@@ -10,7 +10,7 @@ def wallet_id_from_public_key(public_key):
 def seed_from_hash(hash_string):
     hash_prefix = hash_string[0:16]
     int_hex = ""
-    for i in range(int(len(hash_prefix)/2)):
-        byte_hex = hash_prefix[i*2:(i*2)+2]
+    for i in range(int(len(hash_prefix) / 2)):
+        byte_hex = hash_prefix[i * 2:(i * 2) + 2]
         int_hex = "{0}{1}".format(byte_hex, int_hex)
     return int(int_hex, 16)
